8a3707d94c6803026fafe2c9c96c1b541f8847f6
tranthiquynhnhu0901-hue <tranthiquynhnhu0901@gmail.com>
2026-09-25 14:59:56 +0700

    haon thien

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 // CẤU HÌNH KẾT NỐI SUPABASE
-// Chỉ dán PUBLISHABLE KEY / ANON PUBLIC KEY. Không dùng Secret/Service Role key.
+// Chỉ dùng PUBLISHABLE KEY / ANON PUBLIC KEY.
+// Không dùng Secret Key / Service Role Key.
+
 window.APP_CONFIG = {
   SUPABASE_URL: "https://sqdfsxocajcfoxwaujyr.supabase.co",
-  SUPABASE_ANON_KEY: "DAN_PUBLISHABLE_KEY_VAO_DAY",
+  SUPABASE_ANON_KEY: "sb_publishable_gdHj8vHP-ajPAQTumQAi3w_aj5zJEye",
   DEMO_FALLBACK: false
 };
